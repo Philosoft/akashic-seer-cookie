ec64936c1915023f34c41328ab7e710c2d60c0bb
Alexander Frolov <thephilosoft@gmail.com>
2021-02-08 16:18:18 -0500

    feat(cookie): the simplest possible cookie set

---
+++ src/Controller/CookieController.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Cookie;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class CookieController
+{
+    /**
+     * @Route("/test")
+     */
+    public function testCookie(): Response
+    {
+        $response = new Response();
+        $response->headers->setCookie(Cookie::create('foo', 'bar'));
+        $content = "<html><body><h1>Why don't these cookies work?</h1></body></html>";
+        $response->setContent($content);
+        $response->headers->set('Content-Type', 'text/html');
+        return $response;
+    }
+}
